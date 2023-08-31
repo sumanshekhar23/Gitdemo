39f4f55f645c2b7af8f5c5a54a391bd5efe4297a
sumanshekhar23 <40348937+sumanshekhar23@users.noreply.github.com>
2023-08-31 19:26:38 -0400

    Create index.js

---
+++ .github/actions/sendemail/index.js
@@ -0,0 +1,28 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const nodemailer = require('nodemailer')
+
+async function run() {
+  console.log('Inside custom email sender, host=', core.getInput('smtp-server') );
+
+try {
+  const transporter = nodemailer.createTransport({
+    host: core.getInput('smtp-server') ,
+    port: core.getInput('smtp-port')
+  });
+
+  const mailOptions = {
+    from: core.getInput('from'),
+    to: core.getInput('to'),
+    subject: core.getInput('subject'),
+    text: core.getInput('body')
+  };
+
+  await transporter.sendMail(mailOptions);
+  console.log('Email send ho gaya');
+} catch (error) {
+  console.log('email nahi gaya', error);
+  process.exit(1);
+}
+}
+run();
